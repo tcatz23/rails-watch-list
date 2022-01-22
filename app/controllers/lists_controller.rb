cbd588fd3e7c5341052ebb6961e7cc7872147fd0
Thomas Catz <thomas@spleatapp.com>
2022-01-21 22:23:03 -0300

    movie list terminado

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,27 +1,26 @@
 class ListsController < ApplicationController
 
-def index
-  @lists = List.all
-end
+  def index
+    @lists = List.all
+  end
 
-def show
-  @list = List.find(params[:id])
-end
+  def show
+    @list = List.find(params[:id])
+  end
 
-def new
-  @list = List.new
-end
+  def new
+    @list = List.new
+  end
 
-def create
-  @list = List.new(list_params)
-  @list.save
-  redirect_to list_path(@list)
-end
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to list_path(@list)
+  end
 
 private
 
-def list_params
-  params.require(:list).permit(:name)
-end
-
+  def list_params
+    params.require(:list).permit(:name, :photo)
+  end
 end
